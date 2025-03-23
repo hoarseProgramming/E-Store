b01c70e567efa53136588dcb4140b7e5543c9380
Hampus Eiderström Swahn <hampus.es@gmail.com>
2025-03-23 21:24:33 +0100

    feat/8-add-mudblazor-and-navbar-and-pages

--- EStore.Application/Program.cs
+++ EStore.Application/Program.cs
@@ -4,11 +4,13 @@ using EStore.Application.Interfaces;
 using EStore.Application.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAuthenticationStateProvider, AuthenticationStateProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
