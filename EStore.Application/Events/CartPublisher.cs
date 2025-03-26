19b63eae027c8391f6e3592e17c2d1904265f11a
Hampus Eiderström Swahn <hampus.es@gmail.com>
2025-03-26 13:44:54 +0100

    Update/8-update-store-add-cart

---
+++ EStore.Application/Events/CartPublisher.cs
@@ -0,0 +1,12 @@
+﻿namespace EStore.Application.Events
+{
+    public static class CartPublisher
+    {
+        public static event Action CartStateChanged;
+
+        public static async Task OnCartStateChanged()
+        {
+            CartStateChanged?.Invoke();
+        }
+    }
+}
---
+++ EStore.Application/Mapping/ContractMapping.cs
@@ -0,0 +1,26 @@
+﻿using EStore.Application.ApplicationModels;
+using EStore.Contracts.Responses;
+
+namespace EStore.Application.Mapping
+{
+    public static class ContractMapping
+    {
+        public static StoreProduct MapToStoreProduct(this ProductResponse response)
+        {
+            return new StoreProduct
+            {
+                ProductNumber = response.ProductNumber,
+                ProductName = response.ProductName,
+                Description = response.Description,
+                Price = response.Price,
+                CategoryId = response.CategoryId,
+                IsInAssortment = response.IsInAssortment
+            };
+        }
+
+        public static IEnumerable<StoreProduct> MapToStoreProducts(this ProductsResponse response)
+        {
+            return response.Products.Select(MapToStoreProduct);
+        }
+    }
+}
